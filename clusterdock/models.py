1c7a02ddcfacc589f4f1cc0049a3ada86a8a8eba
RiverRhodesStreamSets <111380599+RiverRhodesStreamSets@users.noreply.github.com>
2023-03-10 10:33:13 -0600

    Update models.py

--- clusterdock/models.py
+++ clusterdock/models.py
@@ -449,7 +449,7 @@ class Node:
                                timeout=30, success=success, failure=failure)
 
         # Add Docker container info to /etc/hosts on non-Mac instances to enable SOCKS5 proxy usage.
-        if sys.platform != 'darwin' and !create_container_kwargs['isMac']:
+        if sys.platform != 'darwin' and not create_container_kwargs['isMac']:
             self._add_node_to_etc_hosts()
 
     def stop(self, remove=True):
